16f77bf858ec2386393da678c4a1c98c562cf8a9
kevinmalaverr <55663587+kevinmalaverr@users.noreply.github.com>
2019-11-20 18:21:07 -0500

    Interfaz casi lista

--- app/src/main/java/com/easysitp/easysitp/ui/listarutas/ListaRutasFragment.java
+++ app/src/main/java/com/easysitp/easysitp/ui/listarutas/ListaRutasFragment.java
@@ -4,19 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.easysitp.easysitp.R;
 
 public class ListaRutasFragment extends Fragment {
 
+    View vista;
+    Button botonVolver;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_listarutas, container, false);
+        vista = inflater.inflate(R.layout.fragment_listarutas, container, false);
+        botonVolver = vista.findViewById(R.id.boton_volver);
 
-        return root;
+        botonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_nav_listarutas_to_nav_inicio);
+            }
+        });
+
+        return vista;
     }
 
 
