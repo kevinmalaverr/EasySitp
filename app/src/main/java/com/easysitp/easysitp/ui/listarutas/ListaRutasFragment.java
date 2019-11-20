91146d2f1a44b65bb5942da404e0acf250b2d6c9
kevinmalaverr <55663587+kevinmalaverr@users.noreply.github.com>
2019-11-20 14:33:28 -0500

    version 1.4

--- app/src/main/java/com/easysitp/easysitp/ui/listarutas/ListaRutasFragment.java
+++ app/src/main/java/com/easysitp/easysitp/ui/listarutas/ListaRutasFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,12 +11,13 @@ import androidx.fragment.app.Fragment;
 import com.easysitp.easysitp.R;
 
 public class ListaRutasFragment extends Fragment {
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_listarutas, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
 
         return root;
     }
+
+
 }
