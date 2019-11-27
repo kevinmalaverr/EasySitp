38846a6c2fee2af05f8db5d13cc6c3476f394eaf
Julian Manosalva <jmanosalva@unal.edu.co>
2019-11-27 16:41:11 -0500

    No abre el app

--- app/src/main/java/com/easysitp/easysitp/MainActivity.java
+++ app/src/main/java/com/easysitp/easysitp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.easysitp.easysitp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -11,20 +16,43 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ImageView photoImageView;
+    private ImageView imageView;
+    private TextView NombreUsuario;
+    private TextView TextCorreo;
+    private TextView TextidUsuario;
+
+    private GoogleSignInClient mSignInClient;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-
-
+        imageView = (ImageView) findViewById(R.id.imageView);
+        NombreUsuario = (TextView) findViewById(R.id.NombreUsuario);
+        TextCorreo = (TextView) findViewById(R.id.TextCorreo);
+        TextidUsuario = (TextView) findViewById(R.id.TextidUusario);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mSignInClient = GoogleSignIn.getClient(this, gso);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -39,6 +67,45 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    setUserData(user);
+                }
+                else{
+                    goLogInScreen();
+                }
+
+            }
+        };
+    }
+
+    private void setUserData(FirebaseUser user) {
+        NombreUsuario.setText(user.getDisplayName());
+        TextCorreo.setText(user.getEmail());
+        TextidUsuario.setText(user.getUid());
+        Glide.with(this).load(user.getPhotoUrl()).into(imageView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+
+    }
+    private void goLogInScreen(){
+        Intent intent = new Intent(this,LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void logOut(View view){
+        FirebaseAuth.getInstance().signOut();
+        goLogInScreen();
     }
 
     @Override
@@ -48,5 +115,15 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener != null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
 
+
+    }
 }
+
+
