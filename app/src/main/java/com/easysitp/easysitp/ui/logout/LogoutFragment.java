e7306c581e80d870fb90cb4c7357e84f9b272f25
kevinmalaverr <55663587+kevinmalaverr@users.noreply.github.com>
2019-11-27 20:02:34 -0500

    cuadro salir

--- app/src/main/java/com/easysitp/easysitp/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/easysitp/easysitp/ui/logout/LogoutFragment.java
@@ -1,5 +1,8 @@
 package com.easysitp.easysitp.ui.logout;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,14 +16,19 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.easysitp.easysitp.LoginActivity;
 import com.easysitp.easysitp.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LogoutFragment extends Fragment {
 
     View vista;
     ConstraintLayout botonVolver;
+    Context context;
 
     private LogoutViewModel logoutViewModel;
+    private GoogleSignInClient mSignInClient;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,9 +36,13 @@ public class LogoutFragment extends Fragment {
                 ViewModelProviders.of(this).get(LogoutViewModel.class);
         vista = inflater.inflate(R.layout.fragment_logout, container, false);
 
-        FirebaseAuth.getInstance().signOut();
-        goLogInScreen();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mSignInClient = GoogleSignIn.getClient(getActivity(), gso);
 
+        mostrarDialogoBasico();
         return vista;
     }
 
@@ -38,5 +50,27 @@ public class LogoutFragment extends Fragment {
         Intent intent = new Intent(getActivity(), LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    private void mostrarDialogoBasico() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Salir");
+        builder.setMessage("¿Quieres salir de la aplicación?")
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseAuth.getInstance().signOut();
+                        goLogInScreen();
+                        mSignInClient.signOut();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(false)
+                .show();
     }
 }
