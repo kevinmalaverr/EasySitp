b89eea26de9aa464a9827549c23bf4d7380b27b0
Julian Manosalva <jmanosalva@unal.edu.co>
2019-12-02 11:42:53 -0500

    Rutas agregadas

--- app/src/main/java/com/easysitp/easysitp/Paraderos.java
+++ app/src/main/java/com/easysitp/easysitp/Paraderos.java
@@ -5,11 +5,17 @@ import java.util.ArrayList;
 public class Paraderos {
 
 
+    public static Parada paradaEntradaCll53;
+    public static Parada paradaBiologia;
+    public static Parada paradaAgrarias;
+    public static Parada paradaEntradaCll44;
+    public static Parada paradaMedicina;
+    public static Parada paradaBiblioteca;
+    public static Parada paradaMuseoArq;
+    public static Parada paradaEntradaCll26;
     public static Parada paradaCyt;
-    public static Parada paradaSindu;
-    public static Parada paradaCade;
-    public static Parada paradaEnfermeria;
-    public static Parada paradaConcha;
+
+
 
     public static Ruta ruta_c24;
     public static Ruta ruta_802;
@@ -18,33 +24,50 @@ public class Paraderos {
     public static ArrayList<Ruta> listaRutas;
 
     public static void ini() {
-        ruta_c24 = new Ruta("C24", "Concha-Sindu");
-        ruta_802 = new Ruta("802", "Enfermería-Cade");
-        ruta_87 = new Ruta("87", "CyT-sindu");
+        ruta_c24 = new Ruta("C24", "Entrada peatonal calle 53-Ciencias agrarias");
+        ruta_802 = new Ruta("802", "Entrada peatonal calle 44-Museo de arquitectura");
+        ruta_87 = new Ruta("87", "Entrada peatonal calle 26-CyT");
+
+        ruta_c24.setCoordenadas(new double[][]{{1,0},{1,0}});
+        ruta_802.setCoordenadas(new double[][]{{1,0},{1,0}});
+        ruta_87.setCoordenadas(new double[][]{{1,0},{1,0}});
 
         listaRutas = new ArrayList<>();
         listaRutas.add(ruta_87);
         listaRutas.add(ruta_802);
         listaRutas.add(ruta_c24);
 
-        paradaCade = new Parada("CADE");
-        paradaCade.add(ruta_c24);
-        paradaCade.add(ruta_802);
+        paradaEntradaCll53 = new Parada("Entrada peatonal calle 53");
+        paradaEntradaCll53.add(ruta_c24);
 
         paradaCyt = new Parada("CyT");
-        paradaCyt.add(ruta_c24);
         paradaCyt.add(ruta_87);
 
-        paradaConcha = new Parada("Concha");
-        paradaConcha.add(ruta_c24);
+        paradaBiologia = new Parada("Biología");
+        paradaBiologia.add(ruta_c24);
 
-        paradaSindu = new Parada("Sindu");
-        paradaSindu.add(ruta_c24);
-        paradaSindu.add(ruta_87);
+        paradaMedicina = new Parada("Medicina");
+        paradaMedicina.add(ruta_c24);
+        paradaMedicina.add(ruta_802);
+        paradaMedicina.add(ruta_87);
 
-        paradaEnfermeria = new Parada("Enfermería");
-        paradaEnfermeria.add(ruta_802);
-        paradaEnfermeria.add(ruta_87);
+        paradaAgrarias = new Parada("Ciencias agrarias");
+        paradaAgrarias.add(ruta_c24);
+
+        paradaEntradaCll44 = new Parada("Entrada peatonal calle 44");
+        paradaEntradaCll44.add(ruta_802);
+
+        paradaBiblioteca = new Parada("Biblioteca central");
+        paradaBiblioteca.add(ruta_802);
+        paradaBiblioteca.add(ruta_87);
+
+        paradaMuseoArq = new Parada("Museo de arquitectura");
+        paradaMuseoArq.add(ruta_802);
+
+        paradaEntradaCll26 = new Parada("Ciencias agrarias");
+        paradaEntradaCll26.add(ruta_87);
+
+
     }
 
     public static String numeroRutas(Parada parada) {
